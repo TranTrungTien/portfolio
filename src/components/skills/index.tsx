db01bdb4cae47fa1d49556c479a4c34548eda365
Tien <ictu@gmail.com>
2022-05-16 18:02:39 +0700

    fix image error

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -4,12 +4,12 @@ const Skills = (props: Props) => {
   return (
     <section id="skills" className="relative h-auto py-10 lg:py-20">
       <img
-        src="/images/vectors/cylinder.svg"
+        src="images/vectors/cylinder.svg"
         alt="Cylinder Vector"
         className="absolute right-0 top-16 sm:top-7 md:top-28 lg:top-7 w-2/3 md:w-50p lg:w-35p pointer-events-none opacity-50 transition-all animate-absoluteright"
       />
       <img
-        src="/images/vectors/l-vector.svg"
+        src="images/vectors/l-vector.svg"
         alt="L Vector"
         className="absolute left-0 md:left-35p bottom-0 block md:hidden lg:block w-75p opacity-50 md:opacity-100 sm:w-1/3 md:w-1/4 lg:w-1/3 pointer-events-none"
       />
